b55d999401b71004990aa481c2d6ba22051bb498
ShiyamGanesh <shiyamganesh2000@gmail.com>
2021-06-15 16:18:55 +0530

    commented user schema

--- src/models/patient.js
+++ src/models/patient.js
@@ -1,107 +1,94 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
-const jwt=require('jsonwebtoken')
-
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const patientSchema = new mongoose.Schema({
-        name:{
-            type:String,
-            require:true,
-            trim:true
-    
-        },
-        
-        email: {
-         type:String,
-         require:true,
-         unique:true,
-         trim:true,
-         lowercase:true,
-         validate(value){
-             if(!validator.isEmail(value))
-             {
-                 throw new Error('Email is invalid')
-             }
-    
-         }
-    
-        },
-        password: {
-            type:String,
-            require:true,
-            validate(value){
-                if(!validator.isStrongPassword(value))
-                {
-                    throw new Error('Password should be strong enough')
-                }
-            }
-    
-        },
-        tokens:[{
-            token:{
-                type:String,
-                require:true
-            }
-        }]
-    })
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
 
-    //hiding private data of user
-    userSchema.methods.getPublicProfile = function () {
-        const user = this
-        const userObject = user.toObject()
-        
-        delete userObject._id
-        delete userObject.password
-        delete userObject.tokens
-        
-        return userObject
-    }
-   
-    //generating jwt tokens
-    patientSchema.methods.generateAuthToken = async function() {
-      const patient = this
-      
-      const token = jwt.sign({ _id:user._id.toString()},'thisisforauthentication')
-       await user.save()
-       return token
-      
-    }
-
-    patientSchema.statics.findByCredentials = async (email,password) => {
-       
-        const user = await User.findOne({ email })
-        if(!user) {
-            throw new Error('Email does not exists')
-        }
-
-        const isMatch = await bcrypt.compare(password,patient.password)
-        if(!isMatch)
-        {
-            throw new Error ('password is incorrect')
-        }
-        return patient
-    }
-
-  //Hashing the password before saving 
-    patientSchema.pre('save', async function(next)
-    {
-      const patient = this
-
-      if(patient.isModified('password')){
-          patient.password = await bcrypt.hash(patient.password,8)
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
       }
+    },
+  },
+  password: {
+    type: String,
+    require: true,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error("Password should be strong enough");
+      }
+    },
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        require: true,
+      },
+    },
+  ],
+});
 
-      next()
+//hiding private data of user
+// userSchema.methods.getPublicProfile = function () {
+//     const user = this
+//     const userObject = user.toObject()
 
-      })
+//     delete userObject._id
+//     delete userObject.password
+//     delete userObject.tokens
 
-    
+//     return userObject
+// }
 
-    
-const patient = mongoose.model('patient',patientSchema)
-    
+//generating jwt tokens
+patientSchema.methods.generateAuthToken = async function () {
+  const patient = this;
 
+  const token = jwt.sign(
+    { _id: user._id.toString() },
+    "thisisforauthentication"
+  );
+  await user.save();
+  return token;
+};
 
+patientSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Email does not exists");
+  }
 
-module.exports= patient
+  const isMatch = await bcrypt.compare(password, patient.password);
+  if (!isMatch) {
+    throw new Error("password is incorrect");
+  }
+  return patient;
+};
+
+//Hashing the password before saving
+patientSchema.pre("save", async function (next) {
+  const patient = this;
+
+  if (patient.isModified("password")) {
+    patient.password = await bcrypt.hash(patient.password, 8);
+  }
+
+  next();
+});
+
+const patient = mongoose.model("patient", patientSchema);
+
+module.exports = patient;
