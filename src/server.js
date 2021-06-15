a99aebdf087e26e5dad98e6c76305b88ffd125f6
ShiyamGanesh <shiyamganesh2000@gmail.com>
2021-06-15 16:08:32 +0530

    practo

--- src/server.js
+++ src/server.js
@@ -5,17 +5,20 @@ const express = require('express')
 
 const userRouter = require('./routers/login')
 const listUsers = require("./routers/listUser")
+const appoinment = require("./routers/appoinment")
+
 
 const app = express()
 app.use(express.json())
 app.use(userRouter)
 app.use(listUsers)
+app.use(appoinment)
 
 
 const port = process.env.PORT || 5000
 
 
 app.listen(port, () => {
-    console.log("server is running")
+    console.log("server is running in port", port)
 
 })
