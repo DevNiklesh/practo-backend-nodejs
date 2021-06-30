793643a0326964c50574155160f7f900bdb00eb5
ShiyamGanesh <shiyamganesh2000@gmail.com>
2021-06-30 18:26:51 +0530

    commit

--- src/server.js
+++ src/server.js
@@ -20,40 +20,8 @@ app.use(listUsers);
 app.use(appoinment);
 app.use(Prescription);
 app.use(UpdateProfile);
-app.use(MedicalRecord);
 
-<<<<<<< HEAD
-const userRouter = require('./routers/login')
-const listUsers = require("./routers/listUser")
-const appoinment = require("./routers/appoinment")
-const Prescription = require('./routers/prescription')
-const  Rating  = require('./routers/rating')
-const MedicalRecord= require('./routers/medicalrecord')
-const UpdateProfile = require("./routers/updateprofile");
-
-
-
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(userRouter)
-app.use(listUsers)
-app.use(appoinment)
-app.use(Prescription)
-app.use(Rating)
-app.use(MedicalRecord)
-app.use(UpdateProfile)
-
-
-
-
-
-const port = process.env.PORT || 5000
-
-=======
 const port = process.env.PORT || 5000;
->>>>>>> fc0fbca33d86fad256bff4242ff06011994d64dd
 
 app.listen(port, () => {
   console.log("server is running in port", port);
